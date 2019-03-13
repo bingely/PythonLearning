89bf961f3d69496d4edd9ec39076c0239630e2e1
linmb <linmb@qq.com>
2019-03-13 11:08:54 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,30 @@
+L = ['Michel','Bibil',"sisi"]
+print(L[0])
+
+r = []
+n = 3
+for i in range(n):
+    r.append(L[i])
+
+print(r)
+
+L[0:3]
+
+
+def now():
+    print('011221')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
